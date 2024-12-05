9123c7305725936f747c0777a1db2fa5515eb608
gyula <gyula@DESKTOP-BCTH3FF>
2024-12-05 21:17:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZ_Beadando
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
